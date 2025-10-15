a3e45ebc3ac827d765f1a003f368843e3d0d189a
BTTBao <lebao.071205@gmail.com>
2025-10-15 21:29:01 +0700

    UP NHE

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,37 +1,22 @@
-
-import { useState } from 'react'
 import './App.css'
 import HeaderWrapper from './components/Header/HeaderWrapper'
-
-function App() {
-
- 
-
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { Routes, Route, Link } from "react-router-dom";
 import AboutUs from "./pages/AboutUs";
-import Header from "./components/Header";
+
 function Home() {
-  return (
-    <div>
-      <h1>Trang chủ</h1>
-      <p>Đây là trang chính.</p>
-    </div>
-  );
+  return ;
 }
 
 function App() {
   return (
-    <Router>
-      <nav >
-        <Link to="/">Home </Link>
-        <Link to="/about"> About Us</Link>
-      </nav>
-      <Header />
+    <>
+      <HeaderWrapper />
+      
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<AboutUs />} />
       </Routes>
-    </Router>
+    </>
   );
 }
 
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,4 +1,4 @@
-import { StrictMode } from 'react'
+import React, { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
@@ -8,10 +8,10 @@ import { BrowserRouter } from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
-
-
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </StrictMode>
 )
