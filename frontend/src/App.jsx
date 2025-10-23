a2de2d0b77b26e593467a7e1720e44e852d6d866
BTTBao <lebao.071205@gmail.com>
2025-10-23 13:54:00 +0700

    su loi

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import './App.css'
+<<<<<<< Updated upstream
 import HeaderWrapper from './components/Header/HeaderWrapper'
 import ProductDetail from './pages/ProductDetail'
 import AppRouter from './router/AppRouter'
@@ -7,6 +8,24 @@ function App() {
   return (
     <>
       <AppRouter/>
+=======
+
+import { Routes, Route, Link } from "react-router-dom";
+import AboutUs from "./pages/AboutUs";
+
+function Home() {
+  return ;
+}
+
+function App() {
+  return (
+    <>
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<AboutUs />} />
+      </Routes>
+>>>>>>> Stashed changes
     </>
   )
 }
