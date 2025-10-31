a33343af3b1ff8d18bfdb0fe75e0308c361d6376
ThuanPham <quocthuan1133@gmail.com>
2025-10-31 23:37:33 +0700

    category

---
+++ backend/Controllers/CategoryController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using backend.Models;
+using Microsoft.EntityFrameworkCore;
+using backend.Data;
+using backend.Entities;
+
+namespace backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoryController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public CategoryController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/category
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Category>>> GetAllCategories()
+        {
+            var categories = await _context.Categories.ToListAsync();
+            return Ok(categories);
+        }
+    }
+}
