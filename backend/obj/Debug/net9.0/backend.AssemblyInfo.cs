2058b0a902dedfdca1495038e55a03b5250c8472
BTTBao <lebao.071205@gmail.com>
2025-11-02 20:36:07 +0700

    up dao dien cua admin

--- backend/obj/Debug/net9.0/backend.AssemblyInfo.cs
+++ backend/obj/Debug/net9.0/backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a8e6607979b231e9dc655b79fb6b9dab41b7a4a2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2020f3d53fedfb7e26447e066ee7c3654e57f230")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
