6ee2e21c71603f291c06bd191870319c4755c726
Lê Nguyễn Anh Dự <dule1028a@gmail.com>
2025-11-09 22:12:46 +0700

    thêm module Cart

---
+++ backend/DTOs/CartItemDto.cs
@@ -0,0 +1,23 @@
+﻿namespace backend.DTOs
+{
+    public class CartItemDto
+    {
+        public int CartItemId { get; set; }
+
+        public int CartId { get; set; }
+
+        public int ProductId { get; set; }
+
+        public int? VariantId { get; set; }
+
+        public int? Quantity { get; set; }
+
+        public DateTime? AddedAt { get; set; }
+
+        // Thêm các trường hiển thị
+        public string? ProductName { get; set; }
+        public decimal? Price { get; set; }
+        public string? ImageUrl { get; set; }
+        public string? VariantName { get; set; }
+    }
+}
