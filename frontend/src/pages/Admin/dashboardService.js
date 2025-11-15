f89ebbb0d469c840b97b6adfb896046b4d54dbd1
Trannguyen08 <trannguyen08py@gmail.com>
2025-11-15 12:01:45 +0700

    fix dashboard và database

--- frontend/src/pages/Admin/dashboardService.js
+++ frontend/src/pages/Admin/dashboardService.js
@@ -12,17 +12,6 @@ const apiClient = axios.create({
   },
 });
 
-apiClient.interceptors.response.use(
-  (response) => response,
-  (error) => {
-    if (error.response?.status === 401) {
-      localStorage.removeItem('token');
-      window.location.href = '/login';
-    }
-    return Promise.reject(error);
-  }
-);
-
 
 const dashboardService = {
   /**
